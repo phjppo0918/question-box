a967abecced3e83c93a336f8a496c96c70b7f3b6
Hyeonjun Park <phjppo0918@gmail.com>
2023-09-28 00:30:52 +0900

    feat(front) : signupForm에서 비밀번호 입력 제거

--- front/src/SignUpForm.js
+++ front/src/SignUpForm.js
@@ -8,8 +8,6 @@ const SignUpForm = () => {
     email: "",
     name: "",
     nickname: "",
-    password: "",
-    confirmPassword: "",
   });
 
   const handleChange = (e) => {
@@ -20,16 +18,10 @@ const SignUpForm = () => {
     });
   };
 
-  const [passwordMatch, setPasswordMatch] = useState(true);
   const navigate = useNavigate();
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    // 비밀번호와 비밀번호 확인이 일치하는지 확인
-    if (formData.password !== formData.confirmPassword) {
-      setPasswordMatch(false);
-      return;
-    }
     // 여기서 폼 데이터를 처리하는 코드를 작성합니다.
     // formData 객체에 사용자가 입력한 값이 저장되어 있습니다.
     axios({
@@ -37,9 +29,6 @@ const SignUpForm = () => {
       method: "post",
       data: formData,
     }).then((response) => navigate("/login"));
-
-    // 비밀번호 일치 여부 초기화
-    setPasswordMatch(true);
   };
 
   return (
@@ -79,35 +68,6 @@ const SignUpForm = () => {
                 required
               />
             </Form.Group>
-
-            <Form.Group controlId="password">
-              <Form.Label>비밀번호</Form.Label>
-              <Form.Control
-                type="password"
-                name="password"
-                value={formData.password}
-                onChange={handleChange}
-                required
-              />
-            </Form.Group>
-
-            <Form.Group controlId="confirmPassword">
-              <Form.Label>비밀번호 확인</Form.Label>
-              <Form.Control
-                type="password"
-                name="confirmPassword"
-                value={formData.confirmPassword}
-                onChange={handleChange}
-                required
-              />
-            </Form.Group>
-
-            {/* 비밀번호 일치 여부 메시지 */}
-            {!passwordMatch && (
-              <Alert variant="danger">
-                비밀번호와 비밀번호 확인이 일치하지 않습니다.
-              </Alert>
-            )}
 
             <Button variant="primary" type="submit">
               가입하기
